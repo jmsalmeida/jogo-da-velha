3d1ff77b645a05bd32f4e6344c5f46ddaa333c82
James Almeida <james.almeida.ti@gmail.com>
2018-06-19 18:25:44 -0300

    Finalizar jogo da velha JS

--- scripts.js
+++ scripts.js
@@ -4,28 +4,77 @@ let botaoLimpar = document.querySelector('button');
 let jogada = 1;
 
 function fabricarJogada() {
-    if(this.innerHTML==''){
+    if(this.innerHTML===''){
         if(jogada%2!=0){
             this.innerHTML = 'X';
         }else{
             this.innerHTML = 'O';
         }
         jogada++;
+        encerrarJogo();
+    }
+    
+}
+
+function compararCasas(casa1, casa2, casa3){
+    if(casa1.innerHTML === casa2.innerHTML && casa2.innerHTML === casa3.innerHTML){
+        alert("ACERTOU FDPT GANHOW DIZZZGRAÇA");
     }
 }
 
-function clicar(div) {
-    div.addEventListener('click',fabricarJogada);
+function encerrarJogo(){
+    if(jogoDaVelha[0].innerHTML !== "" && jogoDaVelha[1].innerHTML !== "" && jogoDaVelha[2].innerHTML !== ""){
+        compararCasas(jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2]);
+    }
+    
+    if(jogoDaVelha[0].innerHTML !== "" && jogoDaVelha[3].innerHTML !== "" && jogoDaVelha[6].innerHTML !== ""){
+        compararCasas(jogoDaVelha[0], jogoDaVelha[3], jogoDaVelha[6]);
+    }
+
+    if(jogoDaVelha[0].innerHTML !== "" && jogoDaVelha[4].innerHTML !== "" && jogoDaVelha[8].innerHTML !== ""){
+        compararCasas(jogoDaVelha[0], jogoDaVelha[4], jogoDaVelha[8]);
+    }
+
+    if(jogoDaVelha[3].innerHTML !== "" && jogoDaVelha[4].innerHTML !== "" && jogoDaVelha[5].innerHTML !== ""){
+        compararCasas(jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5]);
+    }
+
+    if(jogoDaVelha[6].innerHTML !== "" && jogoDaVelha[7].innerHTML !== "" && jogoDaVelha[8].innerHTML !== ""){
+        compararCasas(jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
+    }
+
+    if(jogoDaVelha[1].innerHTML !== "" && jogoDaVelha[4].innerHTML !== "" && jogoDaVelha[7].innerHTML !== ""){
+        compararCasas(jogoDaVelha[1], jogoDaVelha[4], jogoDaVelha[7]);
+    }
+
+    if(jogoDaVelha[2].innerHTML !== "" && jogoDaVelha[5].innerHTML !== "" && jogoDaVelha[8].innerHTML !== ""){
+        compararCasas(jogoDaVelha[2], jogoDaVelha[5], jogoDaVelha[8]);
+    }
+
+    if(jogoDaVelha[2].innerHTML !== "" && jogoDaVelha[4].innerHTML !== "" && jogoDaVelha[6].innerHTML !== ""){
+        compararCasas(jogoDaVelha[2], jogoDaVelha[4], jogoDaVelha[6]);
+    }   
+    
+}
+
+function clicar() {
+    
+    for (div of jogoDaVelha){
+        div.onclick = fabricarJogada;
+    }
+    
 }
 
 function limparTabuleiro() {
-    jogoDaVelha.forEach(limparCampo);
+    for (div of jogoDaVelha){
+        div.innerHTML = "";
+    }
+
     jogada = 1;
 }
 
-function limparCampo(div) {
-    div.innerHTML='';
-}
+clicar();
 
-jogoDaVelha.forEach(clicar);
-botaoLimpar.addEventListener('click',limparTabuleiro);
+
+
+botaoLimpar.onclick = limparTabuleiro;
